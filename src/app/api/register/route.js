fb22ede9d0f7c6b882a8426e57243bf80434c199
farsyamzhr <farsyameidiana@gmail.com>
2025-08-28 16:43:35 +0700

    integration registration user

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,11 +1,52 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
 
 export async function POST(req) {
-  const { name, email, password } = await req.json();
+  try {
+    const { name, email, password } = await req.json();
 
-  if (!name || !email || !password) {
-    return NextResponse.json({ message: "All fields are required" }, { status: 400 });
-  }
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 }
+      );
+    }
 
-  return NextResponse.json({ message: "Registrasion success" }, { status: 201 });
+    // cek email
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "Email already registered" },
+        { status: 400 }
+      );
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // saved to database
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Registration success", user: newUser },
+      { status: 201 }
+    );
+    } catch (error) {
+    return NextResponse.json(
+        { message: "Internal server error", error: error.message },
+        { status: 500 }
+    );
+    }
 }
